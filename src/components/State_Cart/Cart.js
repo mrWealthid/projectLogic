b72ab896e613fc47b6ca9496f2c5ac1d129f2f9d
wealth iduwe <wealthiduwe@gmail.com>
2022-07-14 14:46:26 +0100

    Integrated Remita payment gateway using react

--- src/components/State_Cart/Cart.js
+++ src/components/State_Cart/Cart.js
@@ -2,15 +2,47 @@ import React, { useState, useEffect } from "react";
 import { products } from "../../data/data";
 import CartItems from "./CartItems";
 import { FaShoppingBag } from "react-icons/fa";
+import RemitaPayment from "react-remita";
 
 const Cart = () => {
   const [cart, setCart] = useState(products);
   const [cartlength, setCartLength] = useState(null);
-  const [total, setTotal] = useState(null);
+  const [total, setTotal] = useState("");
   const [alert, setAlert] = useState({
     type: false,
     msg: "",
   });
+
+  const [userDetails, setUserDetails] = useState({
+    firstName: "Wealth",
+    lastName: "Iduwe",
+    email: "wealthIduwe@gmail.com",
+  });
+
+  const [paymentData, setpaymentData] = useState({
+    key: "QzAwMDAyNzEyNTl8MTEwNjE4NjF8OWZjOWYwNmMyZDk3MDRhYWM3YThiOThlNTNjZTE3ZjYxOTY5NDdmZWE1YzU3NDc0ZjE2ZDZjNTg1YWYxNWY3NWM4ZjMzNzZhNjNhZWZlOWQwNmJhNTFkMjIxYTRiMjYzZDkzNGQ3NTUxNDIxYWNlOGY4ZWEyODY3ZjlhNGUwYTY=",
+    customerId: "",
+    firstName: userDetails.firstName,
+    lastName: userDetails.lastName,
+    email: userDetails.email,
+    amount: null,
+    narration: "test",
+  });
+
+  useEffect(() => {
+    setpaymentData((prevState) => ({
+      ...prevState,
+      amount: total,
+    }));
+  }, [total]);
+
+  useEffect(() => {
+    setUserDetails({
+      firstName: "Wealth",
+      lastName: "Iduwe",
+      email: "wealthIduwe@gmail.com",
+    });
+  }, []);
 
   useEffect(() => {
     const alertTimer = setTimeout(() => {
@@ -32,6 +64,11 @@ const Cart = () => {
       setTotal(cart.reduce((acc, item) => acc + item.price * item.amount, 0));
     };
     totalAmount();
+
+    setpaymentData((prevState) => ({
+      ...prevState,
+      total: total,
+    }));
   }, [cart, total]);
 
   const customAlert = (type, msg) => {
@@ -66,6 +103,22 @@ const Cart = () => {
     setCart(cart.filter((item) => item.id !== id));
   };
 
+  let data = {
+    ...paymentData,
+    onSuccess: function (response) {
+      // function callback when payment is successful
+      console.log("callback Successful Response", response);
+    },
+    onError: function (response) {
+      // function callback when payment fails
+      console.log("callback Error Response", response);
+    },
+    onClose: function () {
+      // function callback when payment modal is closed
+      console.log("closed");
+    },
+  };
+
   return (
     <div className="flex flex-col flex-wrap">
       <header className="flex items-center justify-between bg-gray-700 text-white p-4">
@@ -96,15 +149,24 @@ const Cart = () => {
             />
           ))}
 
-          <button
-            className="bg-white ring-red-400 ring-1 p-3 mt-10 hover:bg-red-700 ease-linear transition-all duration-1000 hover:text-white  rounded-lg w-full"
-            onClick={() => {
-              customAlert(true, "Cart Emptied!");
-              setCart([]);
-            }}
-          >
-            Clear Item
-          </button>
+          <div className="flex justify-center items-center gap-2">
+            <RemitaPayment
+              remitaData={data}
+              className="bg-green-500 proceed  p-3 mt-10 hover:bg-green-700 ease-linear transition-all duration-1000 text-white hover:text-white  w-1/6  rounded-lg "
+              text="Pay with Remita" //text to show on button
+              // add a 'live' prop to use the live urls/keys
+            />
+
+            <button
+              className="bg-white ring-red-400 ring-1 p-3 mt-10 hover:bg-red-700 ease-linear transition-all duration-1000 hover:text-white  rounded-lg w-1/6"
+              onClick={() => {
+                customAlert(true, "Cart Emptied!");
+                setCart([]);
+              }}
+            >
+              Clear Item
+            </button>
+          </div>
 
           <p className=" bg-white text-lg p-5 text-center mt-4 ">
             Total Amount to be Paid : ${total}
